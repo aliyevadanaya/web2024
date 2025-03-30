63d65e9455a0d3a9c51a031646797b5cc1d8a04b
Aliyeva Danaya <a_danaya@kbtu.kz>
2025-03-30 16:00:05 +0600

    lab

--- labka8/api/admin.py
+++ labka8/api/admin.py
@@ -4,5 +4,8 @@ from django.contrib import admin
 from django.contrib import admin
 from .models import Product, Category
 
-admin.site.register(Product)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'count', 'is_active', 'category', 'likes', 'rating')
+
 admin.site.register(Category)
--- labka8/api/models.py
+++ labka8/api/models.py
@@ -1,22 +1,22 @@
 from django.db import models
 
-# Create your models here.
 class Category(models.Model):
-    name = models.CharField(max_length=30)
-    
+    name = models.CharField(max_length=100)
+
     def __str__(self):
         return self.name
 
 class Product(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=200)
     price = models.FloatField()
     description = models.TextField()
     count = models.IntegerField()
     is_active = models.BooleanField()
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
-    
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    image = models.URLField(default='https://via.placeholder.com/150')
+    likes = models.IntegerField(default=0)
+    rating = models.FloatField(default=0.0)
+    link = models.URLField(default='https://example.com')
+
     def __str__(self):
         return self.name
-    
-    
-    
